5ce1e8c1671ec6a7eb0399b95631984dae3de157
Krishavu <158797460+Krishavu@users.noreply.github.com>
2026-01-03 19:29:11 +0530

    Time: 13 ms (41.93%) | Memory: 46.3 MB (40.58%) - LeetSync

--- Easy/424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ Easy/424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,22 +1,18 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int maxl=0;
-        int maxf=0;
-        int l=0; int r=0;
-        int[] hsh= new int[26];
+        int l=0;int r=0;
+        int maxf=0; int maxlen=0;
+        int []hash= new int[26];
         while(r<s.length()){
-            hsh[s.charAt(r)-'A']++;
-            maxf=Math.max(maxf,hsh[s.charAt(r)-'A']);
-
-            if((r-l+1-maxf)<=k){
-                maxl=Math.max(maxl,r-l+1);
-            }else{
-                hsh[s.charAt(l)-'A']--;
-                // if(mpp.get(p)==0) mpp.remove(s.charAt(l));
+            hash[s.charAt(r)-'A']++;
+            maxf=Math.max(maxf,hash[s.charAt(r)-'A']);
+            if(r-l+1-maxf>k){
+                hash[s.charAt(l)-'A']--;
                 l++;
             }
+            maxlen=Math.max(maxlen,r-l+1);
             r++;
         }
-        return maxl;
+        return maxlen;
     }
 }
