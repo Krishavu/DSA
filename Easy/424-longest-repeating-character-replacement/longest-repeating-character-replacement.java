c83436ff1993e0b3dba4a092b6a32594b0bb43ad
Krishavu <158797460+Krishavu@users.noreply.github.com>
2026-01-03 12:18:16 +0530

    Time: 42 ms (10.98%) | Memory: 47.1 MB (17.53%) - LeetSync

--- Easy/424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ Easy/424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -5,20 +5,15 @@ class Solution {
         int l=0; int r=0;
         HashMap <Character,Integer> mpp = new HashMap<>();
         while(r<s.length()){
-            if(!mpp.containsKey(s.charAt(r))){
-                mpp.put(s.charAt(r),1);
-            }else{
-            char p=s.charAt(r);
-                mpp.put(s.charAt(r),mpp.get(p)+1);
-            }
+            mpp.put(s.charAt(r),mpp.getOrDefault(s.charAt(r),0)+1);
             maxf=Math.max(maxf,mpp.get(s.charAt(r)));
 
             if((r-l+1-maxf)<=k){
                 maxl=Math.max(maxl,r-l+1);
             }else{
                 char p=s.charAt(l);
-                if(mpp.get(s.charAt(l))==1) mpp.remove(s.charAt(l));
-                else mpp.put(s.charAt(l),mpp.get(p)-1);
+                mpp.put(s.charAt(l),mpp.get(p)-1);
+                if(mpp.get(p)==0) mpp.remove(s.charAt(l));
                 l++;
             }
             r++;
