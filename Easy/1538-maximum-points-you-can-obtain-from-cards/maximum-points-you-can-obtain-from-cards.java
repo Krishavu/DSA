0f7db40fbf8cd6b2a3c36a70cbc75eedf0e587de
Krishavu <158797460+Krishavu@users.noreply.github.com>
2026-01-03 17:57:11 +0530

    Time: 2 ms (99.52%) | Memory: 61.9 MB (16.90%) - LeetSync

--- Easy/1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.java
+++ Easy/1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.java
@@ -1,24 +1,33 @@
 class Solution {
     public int maxScore(int[] cardPoints, int k) {
+        int n = cardPoints.length;
 
-        int lsum=0;
-        for(int i=0;i<k;i++){
-            lsum+=cardPoints[i];
-        }
-        int rsum=0;
-        int msum=0;
-        int n=cardPoints.length;
-        int ri=n-1;
-        int li=k-1;
-        while(lsum!=0){
-            int sum=lsum+rsum;
-            msum=Math.max(msum,sum);
-            lsum-=cardPoints[li--];
-            rsum+=cardPoints[ri--];
-        }
-        int sum=lsum+rsum;
-        msum=Math.max(msum,sum);
-        return msum;
+        // Edge case: pick no cards
+        if (k == 0) return 0;
 
+        // Edge case: pick all cards
+        if (k == n) {
+            int total = 0;
+            for (int x : cardPoints) total += x;
+            return total;
+        }
+
+        // Initial sum: take first k cards from left
+        int currentSum = 0;
+        for (int i = 0; i < k; i++) {
+            currentSum += cardPoints[i];
+        }
+
+        int maxSum = currentSum;
+
+        // Slide: remove from left, add from right
+        int rightIndex = n - 1;
+        for (int i = k - 1; i >= 0; i--) {
+            currentSum -= cardPoints[i];
+            currentSum += cardPoints[rightIndex--];
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
     }
 }
