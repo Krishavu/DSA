1f406c2660a2da269f0b36562362165953f5d35e
Aviral Khandelwal <158797460+Krishavu@users.noreply.github.com>
2026-06-23 22:10:19 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (73.97%) - LeetSync

--- Easy/21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ Easy/21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -9,26 +9,48 @@
  * }
  */
 class Solution {
-public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode dummy, temp;
-        dummy = new ListNode();
-        temp = dummy;
-
-        while(list1!=null && list2!=null){
-            if(list1.val < list2.val){
-                temp.next = list1;
-                list1 = list1.next;
-            }
-            else{
-                temp.next = list2;
-                list2 = list2.next;   
-            }
-            temp = temp.next;
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1==null) return list2;
+        if(list2==null) return list1;
+        if(list1.val > list2.val){
+            ListNode temp = list1;
+            list1=list2;
+            list2=temp;
         }
+        ListNode res=list1;
+        while(list1!=null && list2!=null){
+            ListNode temp= null;
+            while(list1!=null && list1.val<=list2.val){
+                temp=list1;
+                list1=list1.next;
+            }
+            temp.next=list2;
+
+            ListNode temp2=list1;
+            list1=list2;
+            list2=temp2;
+        }
+        return res;
+
+        // ListNode dummy, temp;
+        // dummy = new ListNode();
+        // temp = dummy;
+
+        // while(list1!=null && list2!=null){
+        //     if(list1.val < list2.val){
+        //         temp.next = list1;
+        //         list1 = list1.next;
+        //     }
+        //     else{
+        //         temp.next = list2;
+        //         list2 = list2.next;   
+        //     }
+        //     temp = temp.next;
+        // }
         
-        if(list1!=null) temp.next = list1;
-        if(list2!=null) temp.next = list2;
+        // if(list1!=null) temp.next = list1;
+        // if(list2!=null) temp.next = list2;
         
-        return dummy.next;
+        // return dummy.next;
     }
 };
