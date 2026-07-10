a07d04cd61e51d9cb75f02af1e3f71eea68b392b
Aviral Khandelwal <158797460+Krishavu@users.noreply.github.com>
2026-07-10 11:10:29 +0530

    Time: 6 ms (94.80%) | Memory: 46.5 MB (48.76%) - LeetSync

--- Easy/583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
+++ Easy/583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
@@ -1,18 +1,18 @@
 class Solution {
     public int f(String word1, String word2, int i, int j, int dp[][]){
-        if(i<0 || j<0) return 0;
+        if(i==0 || j==0) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-        if(word1.charAt(i)==word2.charAt(j)) return dp[i][j]=1+f(word1, word2, i-1, j-1,dp);
+        if(word1.charAt(i-1)==word2.charAt(j-1)) return dp[i][j]=1+f(word1, word2, i-1, j-1,dp);
         return dp[i][j]=Math.max(f(word1, word2, i,j-1,dp), f(word1, word2, i-1, j,dp));
     }
     public int minDistance(String word1, String word2) {
         int n=word1.length();
         int m=word2.length();
-        int dp[][]= new int[n][m];
+        int dp[][]= new int[n+1][m+1];
         for( int row[]: dp){
             Arrays.fill(row,-1);
         }
-        int x=f(word1, word2, n-1, m-1,dp);
+        int x=f(word1, word2, n, m,dp);
         return (n-x)+(m-x);
     }
 }
